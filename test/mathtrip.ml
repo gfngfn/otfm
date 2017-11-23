b97146b62bd9b93b3b696ae294e85901ba07f92e
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-11-23 19:45:38 +0900

    dummy

--- test/mathtrip.ml
+++ test/mathtrip.ml
@@ -28,6 +28,30 @@ let ( >>= ) x f = match x with Ok(v) -> f v | Error(_) as e -> e
 let return v = Ok(v)
 
 
+let pp = Format.fprintf
+let fmt = Format.std_formatter
+
+
+let pp_cmap ppf d =
+  let pp_map ppf u gid = pp ppf "@,(%a GID%d)" Otfm.pp_cp u gid in
+  let pp_binding ppf () k (u0, u1) gid = match k with
+  | `Glyph -> for u = u0 to u1 do pp_map ppf u gid done
+  | `Glyph_range -> for i = 0 to (u1 - u0) do pp_map ppf (u0 + i) (gid + i)done
+  in
+  pp ppf "@,@[<v1>(cmap";
+  match Otfm.cmap d (pp_binding ppf) () with
+  | Error _ as e -> e
+  | Ok ((pid, eid, fmt), _) ->
+      pp ppf "@,@[<1>(source@ (platform-id %d)@ (encoding-id %d)\
+              @ (format %d))@])@]" pid eid fmt;
+      Ok ()
+
+
+let pp_mvr fmt mvr =
+  let (main, _) = mvr in
+  pp fmt "%d" main
+
+
 let main () =
   let filename =
     try Sys.argv.(1) with
@@ -40,12 +64,25 @@ let main () =
   in
   Otfm.decoder (`String(src)) >>= function
   | Otfm.SingleDecoder(d) ->
-      let () = print_endline "finish initializing decoder" in
-      Otfm.math d >>= fun _ ->
+(*
+      pp_cmap fmt d >>= fun () ->
+*)
+      Otfm.math d >>= fun math ->
+      let mgi = math.Otfm.math_glyph_info in
+
+      let mic = mgi.Otfm.math_italics_correction in
+      pp fmt "@,@[<v1>(math-italics-correction";
+      mic |> List.iter (fun (gid, mvr) -> pp fmt "@,@[(GID%d %a)@]" gid pp_mvr mvr);
+      pp fmt ")@]";
+
+      let mki = mgi.Otfm.math_kern_info in
+      pp fmt "@,@[<v1>(math-kern-info";
+      mki |> List.iter (fun (gid, mk) -> pp fmt "@,@[(GID%d)@]" gid);
+      pp fmt ")@]";
       return ()
 
   | Otfm.TrueTypeCollection(_) ->
-      let () = print_endline "TrueType Collection" in
+      Format.eprintf "TrueType Collection";
       return ()
 
 
