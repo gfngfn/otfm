7f330dddaeafe0a2d8a393ae2aaefafa08e5e1ce
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2019-02-15 21:23:31 +0900

    fix 'gsubtrip.ml'

--- test/gsubtrip.ml
+++ test/gsubtrip.ml
@@ -47,62 +47,70 @@ let pp_sep fmt () =
 
 
 let decode_gsub scripttag type3tag type4tag d =
-  Otfm.gsub_script d >>= fun scriptlst ->
+  Otfm.gsub_script d >>= function
+  | None ->
+      Format.printf "No GSUB@,";
+      return ([], [])
 
-  Format.printf "@[<v2>@,";
-  Format.printf "all GSUB Script tags:@ @[[%a]@]@,"
-    (Format.pp_print_list ~pp_sep Format.pp_print_string)
-      (scriptlst |> List.map Otfm.gsub_script_tag);
+  | Some(scriptlst) ->
+      Format.printf "@[<v2>@,";
+      Format.printf "all GSUB Script tags:@ @[[%a]@]@,"
+        (Format.pp_print_list ~pp_sep Format.pp_print_string)
+          (scriptlst |> List.map Otfm.gsub_script_tag);
 
-  pickup scriptlst (fun gs -> Otfm.gsub_script_tag gs = scripttag)
-    (`Msg(str "GSUB does not contain Script tag '%s'" scripttag)) >>= fun script ->
-  Otfm.gsub_langsys script >>= fun (langsys_DFLT, _) ->
-  Otfm.gsub_feature langsys_DFLT >>= fun (_, featurelst) ->
+      pickup scriptlst (fun gs -> Otfm.gsub_script_tag gs = scripttag)
+        (`Msg(str "GSUB does not contain Script tag '%s'" scripttag)) >>= fun script ->
+      Otfm.gsub_langsys script >>= fun (langsys_DFLT, _) ->
+      Otfm.gsub_feature langsys_DFLT >>= fun (_, featurelst) ->
 
-  Format.printf "all GSUB Feature tags for '%s', 'DFLT':@ @[[%a]@]@," scripttag
-    (Format.pp_print_list ~pp_sep Format.pp_print_string)
-      (featurelst |> List.map Otfm.gsub_feature_tag);
+      Format.printf "all GSUB Feature tags for '%s', 'DFLT':@ @[[%a]@]@," scripttag
+        (Format.pp_print_list ~pp_sep Format.pp_print_string)
+          (featurelst |> List.map Otfm.gsub_feature_tag);
 
-  pickup featurelst (fun gf -> Otfm.gsub_feature_tag gf = type4tag)
-    (`Msg(str "GSUB does not contain Feature tag '%s' for '%s', 'DFLT'" type4tag scripttag)) >>= fun feature_type4 ->
-  Otfm.gsub feature_type4 ~lig:f_lig [] >>= fun type4ret ->
+      pickup featurelst (fun gf -> Otfm.gsub_feature_tag gf = type4tag)
+        (`Msg(str "GSUB does not contain Feature tag '%s' for '%s', 'DFLT'" type4tag scripttag)) >>= fun feature_type4 ->
+      Otfm.gsub feature_type4 ~lig:f_lig [] >>= fun type4ret ->
 
-  Format.printf "finish '%s'@," type4tag;
+      Format.printf "finish '%s'@," type4tag;
 
-  pickup featurelst (fun gf -> Otfm.gsub_feature_tag gf = type3tag)
-    (`Msg(str "GSUB does not contain Feature tag '%s' for '%s', 'DFLT'" type3tag scripttag)) >>= fun feature_type3 ->
+      pickup featurelst (fun gf -> Otfm.gsub_feature_tag gf = type3tag)
+        (`Msg(str "GSUB does not contain Feature tag '%s' for '%s', 'DFLT'" type3tag scripttag)) >>= fun feature_type3 ->
 
-  Otfm.gsub feature_type3 ~single:f_single ~alt:f_alt [] >>= fun type3ret ->
+      Otfm.gsub feature_type3 ~single:f_single ~alt:f_alt [] >>= fun type3ret ->
 
-  Format.printf "finish '%s'@," type3tag;
-  Format.printf "@]@,";
+      Format.printf "finish '%s'@," type3tag;
+      Format.printf "@]@,";
 
-  return (type3ret, type4ret)
+      return (type3ret, type4ret)
 
 
 let decode_gpos scripttag tag d =
-  Otfm.gpos_script d >>= fun scriptlst ->
+  Otfm.gpos_script d >>= function
+  | None ->
+      Format.printf "no GPOS@,";
+      return []
 
-  Format.printf "@[<v2>@,";
-  Format.printf "all GPOS Script tags:@ @[[%a]@]@,"
-    (Format.pp_print_list ~pp_sep Format.pp_print_string)
-      (scriptlst |> List.map Otfm.gpos_script_tag);
+  | Some(scriptlst) ->
+      Format.printf "@[<v2>@,";
+      Format.printf "all GPOS Script tags:@ @[[%a]@]@,"
+        (Format.pp_print_list ~pp_sep Format.pp_print_string)
+          (scriptlst |> List.map Otfm.gpos_script_tag);
 
-  pickup scriptlst (fun script -> Otfm.gpos_script_tag script = scripttag)
-    (`Msg("GPOS does not contain Script tag 'latn'")) >>= fun script_latn ->
-  Otfm.gpos_langsys script_latn >>= fun (langsys_DFLT, _) ->
-  Otfm.gpos_feature langsys_DFLT >>= fun (_, featurelst) ->
+      pickup scriptlst (fun script -> Otfm.gpos_script_tag script = scripttag)
+        (`Msg("GPOS does not contain Script tag '" ^ scripttag ^ "'")) >>= fun script_latn ->
+      Otfm.gpos_langsys script_latn >>= fun (langsys_DFLT, _) ->
+      Otfm.gpos_feature langsys_DFLT >>= fun (_, featurelst) ->
 
-  Format.printf "all GPOS Feature tags for '%s', 'DFLT':@ @[[%a]@]@," scripttag
-    (Format.pp_print_list ~pp_sep Format.pp_print_string)
-      (featurelst |> List.map Otfm.gpos_feature_tag);
+      Format.printf "all GPOS Feature tags for '%s', 'DFLT':@ @[[%a]@]@," scripttag
+        (Format.pp_print_list ~pp_sep Format.pp_print_string)
+          (featurelst |> List.map Otfm.gpos_feature_tag);
 
-  Format.printf "@]@,";
+      Format.printf "@]@,";
 
-  pickup featurelst (fun feature -> Otfm.gpos_feature_tag feature = tag)
-    (`Msg(str "GPOS does not contain Feature tag '%s' for '%s'" tag scripttag)) >>= fun feature_kern ->
-  Otfm.gpos feature_kern ~single1:f_single1 ~single2:f_single2 ~pair1:f_pair1 ~pair2:f_pair2 ~markbase1:f_markbase1 ~markmark1:f_markmark1 [] >>= fun gposres ->
-  return gposres
+      pickup featurelst (fun feature -> Otfm.gpos_feature_tag feature = tag)
+        (`Msg(str "GPOS does not contain Feature tag '%s' for '%s'" tag scripttag)) >>= fun feature_kern ->
+      Otfm.gpos feature_kern ~single1:f_single1 ~single2:f_single2 ~pair1:f_pair1 ~pair2:f_pair2 ~markbase1:f_markbase1 ~markmark1:f_markmark1 [] >>= fun gposres ->
+      return gposres
 
 
 let main filename script type3tag type4tag gpostag =
