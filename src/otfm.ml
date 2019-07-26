7fbbb1d132727edd36d88223b8676d3ccdf7fb36
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2019-07-27 03:12:57 +0900

    separate 'OtfTag' from 'Otfm'

--- src/otfm.ml
+++ src/otfm.ml
@@ -21,99 +21,11 @@ module IntMap = Map.Make
     let compare i j = i - j
   end)
 
-
 (* -- OpenType tags -- *)
 
-type tag = wint
+module Tag = OtfTag
 
-module Tag = struct
-  type t = tag
-
-  (* -- OpenType version tags -- *)
-
-  let v_wOFF = !%% 0x774F4646L
-  let v_OTTO = !%% 0x4F54544FL
-  let v_ttcf = !%% 0x74746366L
-  let v_true = !%% 0x74727565L (* -- may happen in the wild. -- *)
-
-  (* -- Required common tables tags -- *)
-
-  let cmap = !%% 0x636D6170L
-  let head = !%% 0x68656164L
-  let hhea = !%% 0x68686561L
-  let hmtx = !%% 0x686D7478L
-  let maxp = !%% 0x6D617870L
-  let name = !%% 0x6E616D65L
-  let os2  = !%% 0x4F532F32L
-  let post = !%% 0x706F7374L
-
-  let t_common = [ cmap; head; hhea; hmtx; maxp; name; os2; post ]
-
-  (* -- TTF font table tags -- *)
-
-  let cvt  = !%% 0x63767420L
-  let fpgm = !%% 0x6670676DL
-  let glyf = !%% 0x676C7966L
-  let loca = !%% 0x6C6F6361L
-  let prep = !%% 0x70726570L
-
-  (* -- CFF font table tags -- *)
-
-  let cff  = !%% 0x43464620L
-  let vorg = !%% 0x564F5247L
-
-  (* -- Bitmap glyph tables -- *)
-
-  let ebdt = !%% 0x45424454L
-  let eblc = !%% 0x45424C43L
-  let ebsc = !%% 0x45425343L
-
-  (* -- Optional tables -- *)
-
-  let dsig = !%% 0x44534947L
-  let gasp = !%% 0x67617370L
-  let hdmx = !%% 0x68646D78L
-  let kern = !%% 0x6B65726EL
-  let ltsh = !%% 0x4C545348L
-  let pclt = !%% 0x50434C54L
-  let vdmx = !%% 0x56444D58L
-  let vhea = !%% 0x76686561L
-  let vmtx = !%% 0x766D7478L
-
-  (* -- Advanced Open Type font layout tables -- *)
-
-  let base = !%% 0x42415345L
-  let gdef = !%% 0x47444546L
-  let gpos = !%% 0x47504F53L
-  let gsub = !%% 0x47535542L
-  let jstf = !%% 0x4A535446L
-  let math = !%% 0x4d415448L
-
-  (* -- Functions -- *)
-
-  let of_bytes s =
-    let open WideInt in
-      if String.length s <> 4 then
-        invalid_arg(err_invalid_tag s)
-      else
-        let b0 = of_byte (String.get s 0) in
-        let b1 = of_byte (String.get s 1) in
-        let b2 = of_byte (String.get s 2) in
-        let b3 = of_byte (String.get s 3) in
-        (b0 lsl 24) lor (b1 lsl 16) lor (b2 lsl 8) lor b3
-
-
-  let to_bytes t =
-    let (c0, c1, c2, c3) = cut_uint32_unsafe t in
-    Printf.sprintf "%c%c%c%c" c0 c1 c2 c3
-
-
-  let to_wide_int x = x
-  let of_wide_int x = x
-
-  let compare = Pervasives.compare
-  let pp ppf t = pp ppf "'%s'" (to_bytes t)
-end
+type tag = Tag.t
 
 (* -- Unicode code points -- *)
 
